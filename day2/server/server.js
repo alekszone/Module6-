1d1c343da367baabbf52ecc650202e582a24b7f1
alekszone <50597224+alekszone@users.noreply.github.com>
2020-07-08 17:09:24 +0200

    new

---
+++ day2/server/server.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const cors = require("cors")
+// const { join } = require("path")
+const listEndpoints = require("express-list-endpoints")
+const mongoose = require("mongoose")
+
+const students = require("./students/students/students")
+
+
+const {
+  notFoundHandler,
+  badRequestHandler,
+  genericErrorHandler,
+} = require("./errorHandlers.js")
+
+const server = express()
+
+const port = process.env.PORT
+
+
+server.use(express.json())
+
+server.use(cors())
+
+server.use("/students", students)
+
+
+
+server.use(badRequestHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
+console.log(listEndpoints(server))
+
+mongoose.connect("mongodb://localhost:27017/students-students", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log("Running on port", port)
+    })
+  )
+  .catch((err) => console.log(err))
---
+++ day2/server/students/students/mongo.js
@@ -0,0 +1,63 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+
+const mongo = new Schema(
+  {
+    _id:{
+     type:Number,
+     requried:true,
+     validate: {
+      validator: async (value) => {
+        const id = await Students.findOne({ _id: value })
+        if (id) {
+          throw new Error("Id exist try new one")
+        }
+      }
+    }
+    },
+    name:{
+      type:String,
+      required:true
+      },
+      surname:{
+        type:String,
+        required:true
+      },
+    email: {
+      type:String,
+      required:true,
+      lowercase:true,
+      validate: {
+        validator: async (value) => {
+          const email = await Students.findOne({ email: value })
+          if (email) {
+            throw new Error("Email exist exist already in database")
+          }
+        }
+      }
+ },
+    dateOfBirth: {type: Date,
+    required:true},
+    country: {type:String,
+    required:true  
+    },
+phoneNumber:{
+  type:Number,
+  required:true,
+  validate: {
+    validator: async (value) => {
+      const phoneNumber = await Students.findOne({ phoneNumber: value })
+      if (phoneNumber) {
+        throw new Error("The number exist exist exist already in database")
+      }
+    }
+  }
+}
+
+
+
+  }
+  
+)
+const Students = mongoose.model("Mongo", mongo)
+module.exports = Students
